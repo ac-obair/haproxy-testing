c46463b50846026a376564d253b153123105cdcd
Jason Leitch <jason.leitch@arnoldclark.com>
2021-01-25 15:23:05 +0000

    adding port config

--- reload.sh
+++ reload.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 VER=$1
-IP=$(ifconfig en0 | grep 'inet\b'|awk '{print$2}')
+KERNEL=$(uname -s)
+#STATS_PORT_TCP=44444
+STATS_PORT_TCP=8080
+ADMIN_STATS_PORT_TCP=44443
+WEB_FRONTEND_TCP=80
+ENCRYPTED_WEB_FRONTEND_TCP=443
+DATABASE1_TCP=3306
+DATABASE2_TCP=3307
+
+case $KERNEL in
+  Darwin)
+    INTERFACE=en0
+    ;;
+  Linux)
+   INTERFACE=ens160
+   ;;
+  *)
+   ;;
+esac
+
+IP=$(ifconfig ${INTERFACE} | grep 'inet\b'|awk '{print$2}')
 
 docker container ls -f "name=haproxy*" --format "{{.ID}}"|xargs docker container stop
 
@@ -17,7 +37,13 @@ docker run --rm -d --name haproxy-${VER} \
 		-v /etc/pki/tls/certs/letsencrypt:/etc/pki/tls/certs/letsencrypt \
 		-v /etc/ssl/certs/ca-bundle.crt:/etc/ssl/certs/ca-bundle.crt \
                 -v socket_vol:/var/lib/haproxy \
-                -p 8080:8080 -p 80:80 -p 443:443 -p 3306:3306 -p 3307:3307 haproxy-${VER}
+                -p 8080:${STATS_PORT_TCP} \
+                -p 4443:${ADMIN_STATS_PORT_TCP} \
+                -p 80:${WEB_FRONTEND_TCP} \
+                -p 443:${ENCRYPTED_WEB_FRONTEND_TCP} \
+                -p 3306:${DATABASE1_TCP} \
+                -p 3307:${DATABASE2_TCP} \
+                haproxy-${VER}
 
 echo; echo started stats page http://${IP}:8080 admin:123
 echo listening http://${IP}:80 https://${IP}:443
