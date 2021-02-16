d4bf36f9f67ca09f4ae505f56d4c6fbaa24c1f56
Jason Leitch <jason.leitch@arnoldclark.com>
2021-02-16 14:47:10 +0000

    cleaning up process flow

--- edge-proxy/readme.md
+++ edge-proxy/readme.md
@@ -3,12 +3,9 @@ build and run different versions of haproxy
 Quickly replace the version of haproxy you're working with. Removes old versions add the one specific, cleans up image and container env. 
 
 ```bash
-./reload 2.2
-./reload 2.2.9
-./reload lts (same as 2.2.9 or latest lts)
-./reload latest
-./reload 2.3
-./reload 1.8
+./start.sh 2.2
+./start.sh 2.2.9
+./start.sh lts    # (same as 2.2.9 or latest lts)
 ```
 You must link the sockets from the named volume with these will be created when the configuration is tested before starting the process 
 ```
@@ -17,4 +14,9 @@ You must link the sockets from the named volume with these will be created when 
 This preserves the host haproxy environment for deploys. Use `./unlink` to fall back to the host haproxy version. 
 ```
 ./unlink
+```
+
+```
+./reload-config.sh # doesn't effect container uptime in memory config reload
+./stop.sh          # stop container --rm flag ensures container is removed when stopped
 ```
--- edge-proxy/start.sh
+++ edge-proxy/start.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-VER=$1
-
-docker container ls -f "name=currently-running-proxy" --format "{{.ID}}"|xargs docker container stop
-
-docker run --rm --name haproxy-${VER}-syntax-check --network host \
+TAG=$1
+docker run --rm --name haproxy-syntax-check --network host \
                 -v /etc/haproxy:/usr/local/etc/haproxy \
                 -v /etc/pki/tls/certs/letsencrypt:/etc/pki/tls/certs/letsencrypt \
                 -v /etc/ssl/certs/ca-bundle.crt:/etc/ssl/certs/ca-bundle.crt \
+                -v socket_vol:/var/lib/haproxy \
                 -v /etc/haproxy/errors:/etc/haproxy/errors \
-                -it haproxy:${VER} haproxy -c -f /usr/local/etc/haproxy/haproxy.cfg
+                -it haproxy:${TAG} haproxy -c -f /usr/local/etc/haproxy/haproxy.cfg
 
 docker run --rm -d --name currently-running-proxy --network host \
 		-v /etc/haproxy:/usr/local/etc/haproxy \
@@ -16,7 +14,7 @@ docker run --rm -d --name currently-running-proxy --network host \
 		-v /etc/ssl/certs/ca-bundle.crt:/etc/ssl/certs/ca-bundle.crt \
                 -v socket_vol:/var/lib/haproxy \
                 -v /etc/haproxy/errors:/etc/haproxy/errors \
-                haproxy:${VER}
+                haproxy:${TAG}
 
 echo running containers
 docker container ls
