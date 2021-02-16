aa97a07b88a9786cb48576ddaadc52916144b41c
Jason Leitch <jason.leitch@arnoldclark.com>
2021-02-16 17:01:47 +0000

    no need for -it when checking

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ docker run --rm --name haproxy-syntax-check --network host \
                 -v /etc/ssl/certs/ca-bundle.crt:/etc/ssl/certs/ca-bundle.crt \
                 -v socket_vol:/var/lib/haproxy \
                 -v /etc/haproxy/errors:/etc/haproxy/errors \
-                -it haproxy:${TAG} haproxy -c -f /usr/local/etc/haproxy/haproxy.cfg
+                haproxy:${TAG} haproxy -c -f /usr/local/etc/haproxy/haproxy.cfg
 
 docker run --rm -d --name currently-running-proxy --network host \
 		-v /etc/haproxy:/usr/local/etc/haproxy \
